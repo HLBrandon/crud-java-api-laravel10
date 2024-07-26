98fef645a634bdbddfaad3dbd29fe967a8910681
HLBrandon <bran17052003@gmail.com>
2024-07-26 18:23:54 -0500

    Se modificaron los JOptionPane

--- src/Controller/StudentController.java
+++ src/Controller/StudentController.java
@@ -27,7 +27,7 @@ public class StudentController implements ActionListener {
 
     public void run() {
 
-        view.setTitle("Simple CRUD with API Laravel 10");
+        view.setTitle("Simple CRUD with API Laravel");
         view.setResizable(false);
         view.setLocationRelativeTo(null);
         view.setVisible(true);
